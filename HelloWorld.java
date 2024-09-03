5986fc8179e60bd54aa0ee163bfeb220f61bd3f6
RaymonIdle <variluoxx@gmail.com>
2024-09-03 11:18:46 -0700

    sigma

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,20 @@
 public class HelloWorld
 {
   public static void main(String[] args)
-  {
-    System.out.print("Hello world!");
+  { 
+    int loopNumber = 10;
+    print(loopNumber);
+  }
+
+  public static void print(int loopNumber){
+    for (int i = 0; i < loopNumber; i++) {
+    System.out.println(" =====   ===    =====    ==      ==       =        =");
+    System.out.println("=         =    =         =  =  =  =      = =       =");
+    System.out.println(" =====    =    =   ===   =    =   =     =   =      =");
+    System.out.println("      =   =    =     =   =        =    = = = =     ");
+    System.out.println(" =====   ===    =====    =        =   =       =    =");
+    System.out.println("");
+    System.out.println("");
+    }
   }
 }
